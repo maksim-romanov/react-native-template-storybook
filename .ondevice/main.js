51522e60a7c4780d4a7e685cc29b5b2e28cc6f2b
Maksim Romanov <maksim.romanov@jetrockets.pro>
2022-07-11 01:51:44 +0400

    Updated RN 0.68.1 to 0.69.1

--- .ondevice/main.js
+++ .ondevice/main.js
@@ -1,11 +1,11 @@
 module.exports = {
-  stories: [
-    '../components/**/*.stories.?(ts|tsx|js|jsx)'
-  ],
-   addons: [
+  stories: ['../components/**/*.stories.?(ts|tsx|js|jsx)'],
+  addons: [
     '@storybook/addon-ondevice-notes',
     '@storybook/addon-ondevice-controls',
     '@storybook/addon-ondevice-backgrounds',
     '@storybook/addon-ondevice-actions',
   ],
+
+  stories: ['../components/**/*.stories.?(ts|tsx)'],
 };
