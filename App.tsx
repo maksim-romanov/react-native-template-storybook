5a767e9ca4bde3d5af57a627890d5ab3ec80b045
Maksim Romanov <maksim.romanov@jetrockets.pro>
2022-07-11 21:38:17 +0400

    Some updates

--- App.tsx
+++ App.tsx
@@ -1,2 +1,21 @@
-import StorybookUIRoot from './.ondevice/Storybook';
-export {StorybookUIRoot as default};
+// import StorybookUIRoot from './.ondevice/Storybook';
+// export {StorybookUIRoot as default};
+
+import React from 'react';
+import {Text, View} from 'react-native';
+
+import Config from 'react-native-config';
+
+function App() {
+  React.useEffect(() => {
+    console.log(Config);
+  }, []);
+
+  return (
+    <View>
+      <Text>Hello world</Text>
+    </View>
+  );
+}
+
+export default App;
