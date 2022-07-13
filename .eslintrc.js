0b4c4f4c498ef47a695f0ea4c18010f6ace86323
Maksim Romanov <maksim.romanov@jetrockets.pro>
2022-07-14 01:27:21 +0400

    Updated ESLint

--- .eslintrc.js
+++ .eslintrc.js
@@ -6,27 +6,7 @@ module.exports = {
     '@jetrockets/typescript',
     'plugin:storybook/recommended',
   ],
-  parser: '@typescript-eslint/parser',
-  plugins: ['@typescript-eslint'],
-  rules: {
-    'jsx-quotes': ['error', 'prefer-double'],
-  },
-  overrides: [
-    {
-      files: ['*.ts', '*.tsx'],
-      rules: {
-        '@typescript-eslint/no-shadow': ['error'],
-        'no-shadow': 'off',
-      },
-    },
-  ],
   settings: {
     'import/ignore': ['node_modules/react-native/index\\.js$'],
-    'import/resolver': {
-      node: {
-        extensions: ['.js', '.jsx', '.ts', '.tsx'],
-        moduleDirectory: ['node_modules', 'src/'],
-      },
-    },
   },
 };
