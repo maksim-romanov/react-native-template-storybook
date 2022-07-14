d7c2a0961670e85d19594c68bcf7e09dd0522d1b
Maksim Romanov <maksim.romanov@jetrockets.pro>
2022-07-14 10:26:32 +0400

    Some fixes

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+
 import { StyleSheet, Text, TouchableOpacity, View } from 'react-native';
 
 export type ButtonProps = {
--- src/components/Text/Text.stories.tsx
+++ src/components/Text/Text.stories.tsx
@@ -1,15 +1,16 @@
 // stories/MyButton.stories.tsx
 import React from 'react';
-import {ComponentMeta, ComponentStory} from '@storybook/react';
 
-import {MyButton} from './Text';
+import { type ComponentMeta, type ComponentStory } from '@storybook/react';
+
+import { MyButton } from './Text';
 
 export default {
   title: 'components/testText',
   component: MyButton,
 } as ComponentMeta<typeof MyButton>;
 
-export const Basic: ComponentStory<typeof MyButton> = args => (
+export const Basic: ComponentStory<typeof MyButton> = (args) => (
   <MyButton {...args} />
 );
 
